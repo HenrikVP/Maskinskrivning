15f2e8b1f554f8d877f48bb657ad422d7162c763
HenrikVP <hpo@tec.dk>
2023-11-15 12:15:34 +0100

    Final

--- Io.cs
+++ Io.cs
@@ -4,13 +4,14 @@ namespace Maskinskrivning
 {
     internal class Io
     {
+        static XmlSerializer serializer = new(typeof(int[]));
+        static string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
         public static int[] LoadHighscore()
         {
             int[] highScore = new int[10];
-            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
             using (Stream reader = new FileStream(path + "/highscore.xml", FileMode.Open))
             {
-                XmlSerializer serializer = new(typeof(int[]));
                 highScore = (int[])serializer.Deserialize(reader);
             }
             return highScore;
@@ -18,10 +19,8 @@ namespace Maskinskrivning
 
         public static void SaveHighScore(int[] highScore)
         {
-            string url = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-            using (FileStream stream = File.Create(url + "/highscore.xml"))
+            using (FileStream stream = File.Create(path + "/highscore.xml"))
             {
-                XmlSerializer serializer = new(typeof(int[]));
                 serializer.Serialize(stream, highScore);
             }
         }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using System.Diagnostics;
-using System.Xml.Serialization;
 
 namespace Maskinskrivning
 {
